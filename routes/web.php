a7c34c04f9f9fe4e0860f3b9a2acea777c3a30ba
infusion2k7 <ryan@xedi.com>
2019-01-20 20:22:35 +0000

    Started project feature test. Moved logic from routes into a controller and cleaned up for tests.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/projects', 'ProjectsController@index');
+
+Route::post('/projects', 'ProjectsController@store');
