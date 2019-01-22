ccf8867c23aef25212c8832479cd64c8bad28627
XEDI-RYAN\Ryan <ryan@xedi.com>
2019-01-22 15:14:27 +0000

    Created App\Project new project tests

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -16,8 +16,13 @@ class ProjectsController extends Controller
 
     public function store()
     {
-    	Project::create(request(['title', 'description']));
+        // validate
+        $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
 
+        //persist
+    	Project::create($attributes);
+
+        // redirect
     	return redirect('/projects');
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ProjeX
                 </div>
 
                 <div class="links">
