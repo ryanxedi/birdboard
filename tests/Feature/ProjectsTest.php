c34a1c69fb60a9433c48d59876a6971f95058bc6
infusion2k7 <ryan@xedi.com>
2019-01-22 21:11:31 +0000

    Secured projects to the authenticated user. Added a snazzy logo

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -12,11 +12,26 @@ class ProjectsTest extends TestCase
 
     /** @test */
 
-    public function only_authenticated_users_can_create_projects()
+    public function guests_cannot_create_projects()
     {
 
         $attributes = factory('App\Project')->raw();
         $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+    /** @test */
+
+    public function guests_cannot_view_projects()
+    {
+        $this->get('/projects')->assertRedirect('login');
+    }
+
+    /** @test */
+
+    public function guests_cannot_view_a_single_project()
+    {
+    	$project = factory('App\Project')->create();
+        $this->get($project->path())->assertRedirect('login');
     }
 
    /** @test */
@@ -44,12 +59,25 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_user_can_view_a_project()
     {
+    	$this->be(factory('App\User')->create());
+
         $this->withoutExceptionHandling();
 
-        $project = factory('App\Project')->create();
+        $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
+        
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
+    }
+
+    /** @test */
+    public function an_authenticated_user_cannot_view_the_projects_of_others()
+    {
+   		$this->be(factory('App\User')->create());
+
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertStatus(403);
     }
 
     /** @test */
