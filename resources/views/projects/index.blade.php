1bd533b6cf6a0ad6638f5b2bf721af9ae8b93e25
XEDI-RYAN\Ryan <ryan@xedi.com>
2019-01-24 17:43:22 +0000

    Fixed webmix npm issue. Started styling the projects index.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,17 +2,26 @@
 
 @section ('content')
 
-    <div class="flex items-center mb-3" style="display: flex; align-items: center;">
-        <h1 class="mr-auto" style="margin-right:auto">All ProjeX</h1>
-        <a href="/projects/create">New project</a>
-    </div>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-center w-full">
+            <h2 class="text-grey text-sm font-normal">My Projects</h2>
+            <a href="/projects/create" class="button">Add Project</a>
+        </div>
+    </header>
 
-    <ul>
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse ($projects as $project)
-            <li><a href="{{ $project->path() }}">{{ $project->title }}</a></li>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded-lg shadow" style="height:200px">
+                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4">
+                        <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+                    </h3>
+                    <div class="text-grey">{{ str_limit($project->description, 150) }}</div>
+                </div>
+            </div>
         @empty
-            No projects yet
+            <div>No projects yet</div>
         @endforelse
-    </ul>
+    </main>    
 
 @endsection
