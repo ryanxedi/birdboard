2d6b59975276a2eb52b0aca23622ce7e8e2e6685
XEDI-RYAN\Ryan <ryan@xedi.com>
2019-01-25 12:01:49 +0000

    Styled the project show page.

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,8 +1,39 @@
 @extends ('layouts.app')
 
 @section ('content')
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <p class="text-grey text-sm font-normal">
+                <a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+            </p>
+            <a href="/projects/create" class="button">Add Project</a>
+        </div>
+    </header>
 
-    <h1>{{ $project->title }}</h1>
-    <div>{{ $project->description }}</div>
-    <a href="/projects">Go Back</a>
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 px-3 mb-8">
+                <div class="mb-6">
+                    <h2 class="text-grey text-lg font-normal mb-3">Tasks</h2>
+                    <!-- {{-- tasks --}} -->
+                    <div class="card mb-3">Lorem Ipsum</div>
+                    <div class="card mb-3">Lorem Ipsum</div>
+                    <div class="card mb-3">Lorem Ipsum</div>
+                    <div class="card">Lorem Ipsum</div>
+                </div>
+                <div>
+                    <h2 class="text-grey text-lg font-normal mb-3">General Notes</h2>
+                    <!-- {{-- general notes --}} -->
+                    <textarea class="card w-full" style="min-height:200px">Lorem Ipsum</textarea>
+                </div>
+            </div>
+
+            <div class="lg:w-1/4 px-3">
+                @include ('projects.card')
+            </div>
+        </div>
+    </main>
+
+
+
 @endsection
