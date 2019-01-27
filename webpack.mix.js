279033796dbc67f94e5832eb4cb5107c9378b801
infusion2k7 <ryan@xedi.com>
2019-01-27 01:01:42 +0000

    minor updates

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,5 @@ require('laravel-mix-tailwind');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .tailwind();
+    .sass('resources/sass/app.scss', 'public/css')
+    .tailwind();
