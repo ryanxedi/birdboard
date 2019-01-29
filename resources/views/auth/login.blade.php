eb769df740d1489d2d9f52e800d168b3e0c39a71
infusion2k7 <ryan@xedi.com>
2019-01-29 00:49:34 +0000

    Corrected font on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
             <div class="control">
                 <input id="email"
                        type="email"
-                       class="input bg-transparent border border-grey-light rounded p-2 text-xs w-full{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                       class="input bg-transparent border border-grey-light rounded p-2 text-s w-full{{ $errors->has('email') ? ' is-invalid' : '' }}"
                        name="email"
                        value="{{ old('email') }}"
                        required>
@@ -27,7 +27,7 @@
             <div class="control">
                 <input id="password"
                        type="password"
-                       class="input bg-transparent border border-grey-light rounded p-2 text-xs w-full{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                       class="input bg-transparent border border-grey-light rounded p-2 text-s w-full{{ $errors->has('password') ? ' is-invalid' : '' }}"
                        name="password"
                        required>
             </div>
